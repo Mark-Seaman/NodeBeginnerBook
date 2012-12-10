e19e05f402749f563b7efc8187102a716ba6de11
Mark Seaman <mark.seaman@shrinking-world.com>
2012-12-10 09:37:05 -0700

    Formatting

--- code/application/index.js
+++ code/application/index.js
@@ -2,10 +2,10 @@ var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandlers");
 
-var handle = {}
-handle["/"] = requestHandlers.start;
-handle["/start"] = requestHandlers.start;
+var handle        = {}
+handle["/"]       = requestHandlers.start;
+handle["/start"]  = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
-handle["/show"] = requestHandlers.show;
+handle["/show"]   = requestHandlers.show;
 
 server.start(router.route, handle);
